ef5e28b0a01d3ad0cc0af527695acde5b866bcd2
iPPON <ippon1@riseup.net>
2020-05-14 13:05:22 +0200

    code cut down

--- app/src/main/java/com/simonreisinger/gesturerecognition/MainActivity.kt
+++ app/src/main/java/com/simonreisinger/gesturerecognition/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : Activity(), OnTouchListener, CvCameraViewListener2 {
     private var mRgba: Mat? = null
     private var mBlobColorRgba: Scalar? = null
     private var mBlobColorHsv: Scalar? = null
-    private var mDetector: ColorBlobDetector? = null
     private var mSpectrum: Mat? = null
     private var SPECTRUM_SIZE: Size? = null
     private var CONTOUR_COLOR: Scalar? = null
@@ -80,7 +79,6 @@ class MainActivity : Activity(), OnTouchListener, CvCameraViewListener2 {
 
     override fun onCameraViewStarted(width: Int, height: Int) {
         mRgba = Mat(height, width, CvType.CV_8UC4)
-        mDetector = ColorBlobDetector()
         mSpectrum = Mat()
         mBlobColorRgba = Scalar(255.0)
         mBlobColorHsv = Scalar(255.0)
@@ -93,72 +91,11 @@ class MainActivity : Activity(), OnTouchListener, CvCameraViewListener2 {
     }
 
     override fun onTouch(v: View?, event: MotionEvent): Boolean {
-        /*
-        val cols = mRgba!!.cols()
-        val rows = mRgba!!.rows()
-        val xOffset = (mOpenCvCameraView!!.width - cols) / 2
-        val yOffset = (mOpenCvCameraView!!.height - rows) / 2
-        val x = event.getX() as Int - xOffset
-        val y = event.getY() as Int - yOffset
-        Log.i(TAG, "Touch image coordinates: ($x, $y)")
-        if (x < 0 || y < 0 || x > cols || y > rows) return false
-        val touchedRect = Rect()
-        touchedRect.x = if (x > 4) x - 4 else 0
-        touchedRect.y = if (y > 4) y - 4 else 0
-        touchedRect.width = if (x + 4 < cols) x + 4 - touchedRect.x else cols - touchedRect.x
-        touchedRect.height = if (y + 4 < rows) y + 4 - touchedRect.y else rows - touchedRect.y
-        val touchedRegionRgba = mRgba!!.submat(touchedRect)
-        val touchedRegionHsv = Mat()
-        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL)
-
-        // Calculate average color of touched region
-        mBlobColorHsv = Core.sumElems(touchedRegionHsv)
-        val pointCount: Int = touchedRect.width * touchedRect.height
-        if (mBlobColorHsv != null) {
-            //for (i in mBlobColorHsv!!.`val`.indices) {
-            //    mBlobColorHsv!!.`val`[i] /= pointCount
-            //}
-
-            for (item in mBlobColorHsv!!.`val`)
-                item /= pointCount
-
-
-            mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv)
-            Log.i(
-                TAG,
-                "Touched rgba color: (" + mBlobColorRgba!!.`val`[0] + ", " + mBlobColorRgba!!.`val`[1] +
-                        ", " + mBlobColorRgba!!.`val`[2] + ", " + mBlobColorRgba!!.`val`[3] + ")"
-            )
-            mDetector?.setHsvColor(mBlobColorHsv!!)
-            Imgproc.resize(
-                mDetector?.mSpectrum,
-                mSpectrum,
-                SPECTRUM_SIZE,
-                0.0,
-                0.0,
-                Imgproc.INTER_LINEAR_EXACT
-            )
-            mIsColorSelected = true
-            touchedRegionRgba.release()
-            touchedRegionHsv.release()
-        }
-        */
         return false // don't need subsequent touch events
     }
 
     override fun onCameraFrame(inputFrame: CvCameraViewFrame): Mat? {
         mRgba = inputFrame.rgba()
-        if (mDetector != null && mRgba != null && mIsColorSelected) {
-            mDetector?.process(mRgba)
-            val contours: List<MatOfPoint> = mDetector!!.mContours
-            Log.e(TAG, "Contours count: " + contours.size)
-            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR)
-            val colorLabel = mRgba!!.submat(4, 68, 4, 68)
-            colorLabel.setTo(mBlobColorRgba)
-            val spectrumLabel =
-                mRgba!!.submat(4, 4 + mSpectrum!!.rows(), 70, 70 + mSpectrum!!.cols())
-            mSpectrum!!.copyTo(spectrumLabel)
-        }
         return mRgba
     }
 
