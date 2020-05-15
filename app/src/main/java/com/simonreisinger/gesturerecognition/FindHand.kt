ac3e3ff5ed819c9b13c9f6870ad9037c88c0d5c1
iPPON <ippon1@riseup.net>
2020-05-15 13:17:05 +0200

    FindHand added

---
+++ app/src/main/java/com/simonreisinger/gesturerecognition/FindHand.kt
@@ -0,0 +1,17 @@
+package com.simonreisinger.gesturerecognition
+// Source: https://medium.com/@muehler.v/simple-hand-gesture-recognition-using-opencv-and-javascript-eb3d6ced28a0
+class FindHand{
+    fun getHandContour(){
+
+    }
+
+    fun DrawHandContour(){
+
+    }
+
+    fun getRoughHull(){
+
+    }
+
+
+}
